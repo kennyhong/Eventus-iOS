ef61514c9cb33df409c18e78ad0d398d1cc38a23
Kieran Cairney <cairneyk@myumanitoba.ca>
2017-01-27 13:33:28 -0600

    init iOS project

---
+++ Eventus/Eventus/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Eventus
+//
+//  Created by Kieran on 2017-01-27.
+//  Copyright © 2017 Kieran Cairney. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		window = UIWindow(frame: UIScreen.main.bounds)
+		guard let window = window else { return false }
+		let viewController = ViewController()
+		window.rootViewController = viewController
+		window.backgroundColor = .white
+		window.makeKeyAndVisible()
+		return true
+	}
+}
---
+++ Eventus/Eventus/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Eventus
+//
+//  Created by Kieran on 2017-01-27.
+//  Copyright © 2017 Kieran Cairney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
